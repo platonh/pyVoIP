e03fc7dd7ba76a9a65528d9c8e5b4cc55bf68431
platonh <platonheivan@gmail.com>
2025-11-11 14:12:39 +0200

    Revert "feat: hangup retries"
    This reverts commit 30d61efb902558b596f4421a59d78acb34dec29e.

--- pyVoIP/SIP/client.py
+++ pyVoIP/SIP/client.py
@@ -1235,29 +1235,15 @@ class SIPClient:
 
     def bye(self, request: SIPMessage) -> None:
         message = self.gen_bye(request)
-
-        attempts = 5
-
-        for i in range(attempts):
-            try:
-                conn = self.sendto(
-                    message,
-                    (
-                        request.headers["Contact"]["host"],
-                        request.headers["Contact"]["port"],
-                    ),
-                )
-                response = SIPMessage.from_bytes(conn.recv(8192))
-                break
-
-            except Exception as e:
-                debug(f"BYE ERROR (attempt {i+1}/{attempts}): {e}")
-                if i == attempts - 1:
-                    raise
-                
-                time.sleep(1)
-                continue
-
+        # TODO: Handle bye to server vs. bye to connected client
+        conn = self.sendto(
+            message,
+            (
+                request.headers["Contact"]["host"],
+                request.headers["Contact"]["port"],
+            ),
+        )
+        response = SIPMessage.from_bytes(conn.recv(8192))
         if response.status == ResponseCode(
             401
         ) or response.status == ResponseCode(407):
